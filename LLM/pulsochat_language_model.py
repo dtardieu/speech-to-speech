5787271463f8060589d6dabb4f833047aefaf82a
dtardieu <damien.tardieu@gmail.com>
2025-02-20 18:19:30 +0100

    avec nouveaux fichiers de config

--- LLM/pulsochat_language_model.py
+++ LLM/pulsochat_language_model.py
@@ -15,14 +15,28 @@ from pulsochat.ChatHandler import ChatHandler
 from pulsochat.ConfigManager import ConfigManager
 from pulsochat.InteractionLogger import InteractionLogger
 
+WHISPER_LANGUAGE_TO_LLM_LANGUAGE = {
+    "en": "english",
+    "fr": "french",
+    "es": "spanish",
+    "zh": "chinese",
+    "ja": "japanese",
+    "ko": "korean",
+    "hi": "hindi",
+    "de": "german",
+    "pt": "portuguese",
+    "pl": "polish",
+    "it": "italian",
+    "nl": "dutch",
+}
+
 
 logger = logging.getLogger(__name__)
 console = Console()
 
-key_file = "/Users/damientardieu/Documents/metamorphy-266a29b4942c.json"
+key_file = "/Users/damientardieu/src/speech-to-speech/metamorphy-266a29b4942c.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_file
 
-translate_client = translate.Client()
 
 CHAT_SIZE = 2000
 
@@ -41,7 +55,7 @@ class PulsochatModelHandler(BaseHandler):
         self.stream = stream
         self.client = ChatHandler(config, api_key, InteractionLogger(log_dir))
         self.chat = Chat(CHAT_SIZE)
-
+        self.translate_client = translate.Client()
         # Register handlers for OSC messages
         if self.osc_server:
             self.osc_server.add_handler("/pulsochat/reset", self._handle_reset)
@@ -65,15 +79,28 @@ class PulsochatModelHandler(BaseHandler):
         logger.debug(prompt)
 
         # Call the response generator
-        prompt_en=translate_client.translate(prompt, target_language="en", format_="text")["translatedText"]
+
+        if language_code != "en":
+            #prompt_en = self.translate_client.translate(prompt, target_language="en", format_="text")["translatedText"]
+            prompt_en=prompt
+        else:
+            prompt_en=prompt
+
         response_generator = self.client.response(prompt_en, self.chat.to_list(), stream=True)
 
+        import time
+
+        start = time.time()
         generated_text = ""
         for chunk in response_generator:
             generated_text += chunk
-            chunk_fr = translate_client.translate(chunk, target_language="fr", format_="text")["translatedText"]
+            if language_code != "en-fr":
+                #chunk_fr = self.translate_client.translate(chunk, target_language=language_code, format_="text")["translatedText"]
+                chunk_fr = chunk
+            else:
+                chunk_fr=chunk
             yield chunk_fr, language_code  # Yielding chunks in streaming mode
-
+        end = time.time()
         if self.osc_client:
             self.send_osc_message("/pulsochat/state", str(self.client.get_current_state()))
 
